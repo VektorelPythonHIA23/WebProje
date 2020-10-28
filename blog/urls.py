801bd04bcf4786992ed958980472ffeb0524ab77
vektorelpython23 <72040541+vektorelpython23@users.noreply.github.com>
2020-10-28 20:02:44 +0200

    28102020

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.gonderListe,name="gonderListe")
+    path('',views.gonderListe,name="gonderListe"),
+    path('detay/<int:pk>',views.gonderiDetay,name="gonderiDetay"),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import GonderiModel
 
 
 def gonderListe(request):
-    return render(request,"blog/gonderilist.html",{})
+    gonderiler = GonderiModel.objects.all()
+    return render(request,"blog/gonderilist.html",{"gonderis":gonderiler})
+
+def gonderiDetay(request,pk):
+    gonderi = get_object_or_404(GonderiModel,pk=pk)
+    return render(request,"blog/gonderidetay.html",{"gonderi":gonderi})
